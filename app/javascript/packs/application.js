31531bc1ca729d63e7423531faba288fbe1f10aa
関目智史 <zou3road@gmail.com>
2022-12-16 08:33:13 +0900

    Add:pie_chart_and_bar_chart

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,10 +7,10 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import "chartkick/chart.js"
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
 
-require("chartkick")
-require("cart.js")
+
